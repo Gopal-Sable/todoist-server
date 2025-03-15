505eabd2d21867efacbd9581c95a7338cb1a66cb
Gopal Sable <sablegopal123@gmail.com>
2025-03-15 11:24:59 +0530

    inserted 1000 users and 1000000 projects and added limit to get query

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -26,19 +26,22 @@ const createProject = async (req, res, next) => {
 
 // get all project or specified id
 const getProjects = async (req, res, next) => {
-    let page = req.query.page;
-    if (Number.isInteger(page) || page < 0) {
-        page = 0;
-    }
-    let sql = "SELECT * FROM projects";
-    if (req.params.id) {
-        if (isNaN(req.params.id)) {
-            return res.status(400).json({ message: "Invalid task ID" });
-        }
-        sql += " WHERE id=?";
-    }
-    sql += ` limit 100 offset ${page * 100 || 0}`;
     try {
+        let page = req.query.page;
+        let limit = 10000;
+        if (Number.isInteger(page) || page < 0) {
+            page = 0;
+        }
+        let sql = "SELECT * FROM projects";
+        let rowCount= "SELCT COUNT(*) FROM projects"
+        if (req.params.id) {
+            if (isNaN(req.params.id)) {
+                return res.status(400).json({ message: "Invalid task ID" });
+            }
+            sql += " WHERE id=?";
+            rowCount +=" WHERE id=?"
+        }
+        sql += ` limit ${limit} offset ${page * limit || 0}`;
         const projects = await db.all(sql, [req.params.id]);
         if (req.params.id && projects.length === 0) {
             return res.status(404).json({ message: "Project not found" });
@@ -46,7 +49,7 @@ const getProjects = async (req, res, next) => {
         return res.status(200).json({
             length: projects.length,
             currPage: page,
-            pages: Math.ceil(projects.length / 100),
+            pages: Math.ceil(projects.length / limit),
             projects,
         });
     } catch (error) {
